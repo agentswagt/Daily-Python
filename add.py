2f28efa4879179234a84daa99610507599dbbe4b
shagatoc@gmail.com <shagatoc@gmail.com>
2020-06-15 08:32:43 +0600

    das

---
+++ add.py
@@ -0,0 +1,9 @@
+import sys
+
+
+
+a = sys.argv[1]
+b = sys.argv[2]
+a = int(a)
+b = int(b)
+print(a + b)
--- web craw.py
+++ web craw.py
@@ -1,7 +1,50 @@
+
 import requests
-url = "https://scblog.netlify.app"
 
-response = requests.get(url)
+while True:
+    url = input("Paste image URL: ")
+    pic_name = input("Enter Image name: ")
+    print("Select File Type")
+    print("[1]PNG")
+    print("[2]JPG")
+    print("[3]Gif")
+    print("[4]Change Url")
+    terminate = False
 
-with open("shagato.html", "w") as f:
-    f.write(response.text)
+    while not terminate:
+        file_format = input("Please Enter Your Option: ")
+
+        if file_format in ["png", "Png", "1", "PNG"]:
+            x = ".png"
+            form = "PNG"
+        elif file_format in ["2", "JPG", "Jpg", "jpg"]:
+            x = ".jpg"
+            form = "JPG"
+        elif file_format in ["3", "GIF", "gif", "Gif"]:
+            x = ".gif"
+            form = "GIF"
+        elif file_format == "4":
+            url = input("Paste image URL: ")
+            pic_name = input("Enter Image name: ")
+            print("Select File Type")
+            print("[1]PNG")
+            print("[2]JPG")
+            print("[3]Gif")
+            print("[4]Change Url")
+            continue
+
+
+
+        elif file_format not in ["3", "GIF", "gif", "Gif", "png", "Png", "1", "PNG", "2", "JPG", "Jpg", "jpg"]:
+            print("Unknown Operation")
+            continue
+
+        response = requests.get(url)
+        with open(pic_name + x, "wb") as f:
+            f.write(response.content)
+            print("Image saved")
+            print("-------------------------------------")
+            print("-Image Details:                      -")
+            print(f"-Name:{pic_name}|Format:{form}      -")
+            print("-------------------------------------")
+
